30da75716f31f071ebe448ace333a1c32de1914d
rogerio <joarisce@outlook>
2023-06-21 19:53:57 +0200

    Ajouter un boutton

--- lib/main.dart
+++ lib/main.dart
@@ -53,18 +53,46 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             BigCard(pair: pair),
-            ElevatedButton(
-              onPressed: () {
-                // print('button pressed !');
-                appState.getNext();
-              },
-              child: Text('Next'),
+            // SizeBox pour séparer
+            SizedBox(height: 10),
+            Row(
+              // Yoo ne t'occupe pas de tout l'espace hozintal = MainAxisAlignment
+              // mais met lui au centre
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        appState.toogleFavorite();
+                      },
+                      icon: Icon(icon),
+                      label: Text('Like'),
+                    ),
+                    SizedBox(width: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        // print('button pressed !');
+                        appState.getNext();
+                      },
+                      child: Text('Next'),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
